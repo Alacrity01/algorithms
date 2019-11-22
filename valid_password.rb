2ce065c6aa98e91d969556daf205041f8081041e
TheAlacrity <brinker.jeff@gmail.com>
2019-11-22 16:07:36 -0600

    added ruby solution for valid_password

--- valid_password.rb
+++ valid_password.rb
@@ -1,27 +1,58 @@
 # Write a function that checks if a user input is a valid password or not. 
 # Valid password rules: 
-  # - Minimum length is 5
-  # - Maximum length is 10
-  # - Contains at least one number
-  # - Contains at least one special character
-  # - Contains no spaces
+  # 1 - Minimum length is 5
+  # 2 - Maximum length is 10
+  # 3 - Contains at least one number
+  # 4 - Contains at least one special character
+  # 5 - Contains no spaces
 
-# def is_valid(password)
-#   if password.length < 5
-#     return false
-#   elsif password.length > 10
-#     return false
-#   end
+def is_valid(password)
+  if password.length < 5 # 1
+    return false
+  elsif password.length > 10 # 2
+    return false
+  elsif !(password =~ /[[:digit:]]/) # 3
+    return false
+  elsif !(password =~ /\W/)
+    return false 
+  elsif password =~ /\s/ # 5
+    return false
+  else
+    return true
+  end
+end
 
-#   number_count = 0
+# p is_valid("$potify36") # should return true
+# p is_valid("$potify360000000") # should return false (length > 10)
+# p is_valid("$potify") # should return false (no number)
+# p is_valid("Spotify36") # should return false (no special character)
+# p is_valid("$potify 36") # should return false (contains space)
+# p is_valid("$po7") # should return false (length < 5)
 
-#   i = 0
-#   password.length.times do
-#     if password[i]
-#     i += 1
-#   end
-    
+def is_valid_user_input()
+  puts "Valid password rules: 
+    1 - Minimum length is 5
+    2 - Maximum length is 10
+    3 - Contains at least one number
+    4 - Contains at least one special character
+    5 - Contains no spaces"
+  print "Please enter your password: "
+  password = gets.chomp()
 
+  if password.length < 5 # 1
+    return false
+  elsif password.length > 10 # 2
+    return false
+  elsif !(password =~ /[[:digit:]]/) # 3
+    return false
+  elsif !(password =~ /\W/)
+    return false 
+  elsif password =~ /\s/ # 5
+    return false
+  else
+    return true
+  end
+end
 
-#   return true
-# end
+p is_valid_user_input()
+
