9d1025ab91e43708bf88fd68cf2b60308ff37828
Jeff Brinker <108890474+spr-jeff-brinker@users.noreply.github.com>
2023-11-13 14:23:10 -0600

    Refactored reeborg_hurdle4.py

--- Python/reeborg_hurdle4.py
+++ Python/reeborg_hurdle4.py
@@ -1,21 +1,11 @@
-def jump():
-    turn_left()
-    move()
-    while wall_on_right():  # for variable wall height
-        move()
-    [turn_left() for i in range(3)]  # turn right
-    move()  # crossing hurdle
-    [turn_left() for i in range(3)]  # turn right
-    while front_is_clear():
-        move()  # descending
-    turn_left()  # face forward
-
-
 while not at_goal():
-    if wall_in_front():
-        jump()
-    else:
+    if front_is_clear() and wall_on_right():
         move()
+    elif not wall_on_right():
+        [turn_left() for i in range(3)]  # turn right
+        move()
+    else:
+        turn_left()
 
-# reeborg_hurdle4.py - completed in 164 moves
+# reeborg_hurdle4.py
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
