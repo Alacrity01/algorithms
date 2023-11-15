6542533d61ea534d004baa0f2eb1b72463b8a1b2
Jeff Brinker <108890474+spr-jeff-brinker@users.noreply.github.com>
2023-11-15 07:40:40 -0600

    Added repeat option

--- Python/caesar_cipher.py
+++ Python/caesar_cipher.py
@@ -1,10 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
 
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
@@ -20,4 +16,13 @@ def caesar(start_text, shift_amount, cipher_direction):
     print(f"The {cipher_direction}d text is {end_text}")
 
 
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+complete = True
+while complete == True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:  ")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    repeat = input(
+        "Do you want to rerun the Caesar Cipher? (Type 'yes' to rerun)/n").lower()
+    if repeat != "yes":
+        complete = False
